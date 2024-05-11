3a0ec17e87344b08c996d62c1757d6733b189dba
Farah Osama <faraho2013@gmail.com>
2024-05-11 07:12:19 -0500

    Adding components

---
+++ src/components/BookingForm.js
@@ -0,0 +1,19 @@
+/*import React from "react"
+
+const BookingForm = () => {
+    return (
+        <header>
+            <section>
+                <form>
+                    <fieldset>
+                        <div>
+                            <label htmlFor="book-date"> Choose Date</label>
+                            <input id="book-date" value={date} type="date" required />
+                        </div>
+                    </fieldset>
+                </form>
+            </section>
+        </header>
+    )
+}
+export default BookingForm*/
