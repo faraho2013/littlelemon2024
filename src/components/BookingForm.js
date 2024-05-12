e83c031122b1584b7ea4ced54525fd3c616ebd3d
Farah Osama <faraho2013@gmail.com>
2024-05-12 18:01:30 -0500

    Booking Form and Confirmed Booking

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,14 +1,54 @@
-/*import React from "react"
+import React, { useState } from "react"
 
-const BookingForm = () => {
+const BookingForm = (props) => {
+    const [date, setDate] = useState("")
+    const [time, setTime] = useState("")
+    const [guests, setGuests] = useState("")
+    const [occasion, setOccasion] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        props.SubmitForm(e)
+    }
+    const handleChange = (e) => {
+        setDate(e)
+        props.dispastch(e)
+    }
     return (
         <header>
             <section>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <fieldset>
                         <div>
-                            <label htmlFor="book-date"> Choose Date</label>
-                            <input id="book-date" value={date} type="date" required />
+                            <label htmlFor="book-date"> Choose Date:</label>
+                            <input id="book-date" value={date} onChange={(e) => handleChange(e.target.value)} type="date" required />
+                        </div>
+
+                        <div>
+                            <label htmlFor="book-time"> Choose time:</label>
+                            <select id="book-time" value={time} onChange={(e) => setTime(e.target.value)}>
+                                <option value="">select a Time</option>
+                                {
+                                    props.availableTimes.availableTimes.map(availableTimes => { return < option key={availableTimes}>{availableTimes}</option> })
+                                }
+                            </select>
+                        </div>
+
+                        <div>
+                            <label htmlFor="book-guests">Number of Guests:</label>
+                            <input id="book-guests" type="number" min={1} value={guests} onChange={(e) => setGuests(e.target.value)} />
+                        </div>
+
+                        <div>
+                            <label htmlFor="book-occasion">Occasion:</label>
+                            <select id="book-occasion" key={occasion} value={occasion} onChange={e => setOccasion(e.target.value)}>
+                                <option>Birthday</option>
+                                <option>Anniversary</option>
+                            </select>
+                        </div>
+
+                        <div className="btnReceive">
+                            <input aria-label="on Click" type="submit" value={"Make your reservation"} />
                         </div>
                     </fieldset>
                 </form>
@@ -16,4 +56,4 @@ const BookingForm = () => {
         </header>
     )
 }
-export default BookingForm*/
+export default BookingForm
